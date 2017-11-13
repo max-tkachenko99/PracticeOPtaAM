91b8d333d25a40f0b15f07dc39d36680eed54a17
max-tkachenko99 <tkachenko.maksi.99@gmail.com>
2017-11-13 15:59:14 +0200

    i delete  std

--- День_1/lesson_1.cpp
+++ День_1/lesson_1.cpp
@@ -4,27 +4,27 @@
 #include <iterator>
 #include <sstream>
 #include <vector>
- 
+ using namespace std;
 int main()
     {
-    std::string line;
-    std::getline(std::cin,line);
-    std::istringstream strs(line);
+    string line;
+    getline(std::cin,line);
+    istringstream strs(line);
     
-    std::vector<std::string> words;
-    std::copy(std::istream_iterator<std::string>(strs),
-        std::istream_iterator<std::string>(),
-        std::back_inserter<std::vector<std::string> >(words));
+    vector<string> words;
+    copy(istream_iterator<string>(strs),
+        istream_iterator<string>(),
+        back_inserter<vector<string> >(words));
  
-    std::string min_str = *words.begin(), max_str = *words.begin();
+   string min_str = *words.begin(), max_str = *words.begin();
  
-    for(std::vector<std::string>::iterator i = words.begin(); i != words.end(); ++i)
+    for(vector<string>iterator i = words.begin(); i != words.end(); ++i)
         {
         if(i->length() > max_str.length())
             max_str = *i;
         if(i->length() < min_str.length())
             min_str = *i;
         }
-    std::cout << std::endl << "The longest word is \"" << max_str << "\"." << "Length is " << max_str.length() << std::endl;
-    std::cout << "The shortest word is \"" << min_str << "\"." << "Length is " << min_str.length() << std::endl;
+    cout << endl << "The longest word is \"" << max_str << "\"." << "Length is " << max_str.length() << endl;
+   cout << "The shortest word is \"" << min_str << "\"." << "Length is " << min_str.length() << endl;
     }
